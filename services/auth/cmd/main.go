3691f1bbbc7a5b90a1c85a3a41d511c825437fe6
hasanm95 <hsncool10@gmail.com>
2026-09-15 21:07:49 +0600

    fix: auth cmd

---
+++ services/auth/cmd/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/hasanm95/pulse/services/auth/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/hasanm95/pulse/services/auth/internal/config"
+	"github.com/hasanm95/pulse/services/auth/internal/database"
+)
+
+type server struct {
+	pb.UnimplementedAuthServiceServer
+}
+
+func (s *server) HealthCheck(ctx context.Context, req *pb.HealthRequest) (*pb.HealthResponse, error) {
+	return &pb.HealthResponse{
+		Status: "ok",
+	}, nil
+}
+
+func main() {
+	ctx := context.Background()
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(cfg.Port)
+
+	pool, err := database.New(ctx, cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+
+	log.Println("Database connected")
+
+	listener, err := net.Listen("tcp", ":"+cfg.Port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterAuthServiceServer(grpcServer, &server{})
+
+	log.Printf("AUTH grpc server started on port %s\n", cfg.Port)
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
+}
