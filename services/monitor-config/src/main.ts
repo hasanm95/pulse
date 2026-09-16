dd1abce339a62aa1c88fa26aa3189490ad4056c1
hasanm95 <hsncool10@gmail.com>
2026-09-17 04:40:21 +0600

    fix: gitignore

--- services/monitor-config/src/main.ts
+++ services/monitor-config/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule, ObserveInstrument } from './app.module.js';
+import { AppModule } from './app.module.js';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
-  await app.listen(3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: "monitor",
+      protoPath: process.env.MONITOR_CONFIG_PROTO_PATH || join(__dirname, '../../dist/proto/monitor-config.proto'),
+      url: process.env.MONITOR_CONFIG_SERVICE_URL || '0.0.0.0:50052',
+    }
+  })
+  await app.listen();
+  console.log('Monitor Config gRPC microservice is listening on port 50052...');
 }
 await bootstrap();
