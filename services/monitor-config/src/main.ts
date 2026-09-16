077f959219247d69cd0199e7d7475b088f87c598
hasanm95 <hsncool10@gmail.com>
2026-09-17 04:01:21 +0600

    monitor config init

---
+++ services/monitor-config/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule, ObserveInstrument } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    instrument: ObserveInstrument,
+  });
+  await app.listen(3001);
+}
+await bootstrap();
