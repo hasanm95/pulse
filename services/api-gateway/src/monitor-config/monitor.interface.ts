2bc5063de9efaf1b2c0c4a5f2f9968e346a52eb2
hasanm95 <hsncool10@gmail.com>
2026-09-18 17:40:29 +0600

    fix: type

--- services/api-gateway/src/monitor-config/monitor.interface.ts
+++ services/api-gateway/src/monitor-config/monitor.interface.ts
@@ -15,8 +15,8 @@ export interface Monitor {
     intervalSeconds: number;
     regions: string[];
     status: string;
-    created_at: string;
-    updated_at: string;
+    createdAt: string;
+    updatedAt: string;
 }
 
 export interface CreateMonitorRequest {
